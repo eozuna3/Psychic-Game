268afad63feb0ceeb8954adf4020473863d6ec85
eozuna3 <eo30239@gmail.com>
2020-02-09 17:41:35 -0600

    Completed all necessary coding and have a working version of the game.

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,19 +1,21 @@
-// Creating variables to hold the number of wins, losses, guesses left, and an array to hold the list of guesses made.
+// Creating variables to hold the number of wins, losses, guesses left, random Letter chosen, and an array to hold the list of guesses made.
 
 var wins = 0;
 var losses = 0;
-var guessLeft = 0;
+var guessLeft = 9;
 var guessArray = [];
+var randomLetter = "";
 
 // Create variables that hold references to the places in the HTML where we want to display things.
-var userGuess = document.getElementById("guessSoFar");
-var winsText = document.getElementById("wins");
-var lossesText = document.getElementById("losses");
-var guessLeftText = document.getElementById("guessLeft");
+var userGuessText = document.getElementById("guessSoFar-Text");
+var winsText = document.getElementById("wins-Text");
+var lossesText = document.getElementById("losses-Text");
+var guessLeftText = document.getElementById("guessLeft-Text");
+var instructionText = document.getElementById("Instructions")
 
 //Function for random letter generator.
 function stringGen(len) {
-  var text = " ";
+  var text = "";
   var charset = "abcdefghijklmnopqrstuvwxyz";
 
   for (var i = 0; i < len; i++)
@@ -22,31 +24,64 @@ function stringGen(len) {
   return text;
 }
 
+//Initial Display Text
+winsText.textContent = "Wins: 0";
+lossesText.textContent = "Losses: 0";
+guessLeftText.textContent = "Guesses left: " + guessLeft;
+userGuessText.textContent = "Your guesses so far: ";
+instructionText.textContent = "Push any letter to start game."
+
+//Initializing the first random letter when the page loads
+randomLetter = stringGen(1);
+console.log(randomLetter);
+
 document.onkeypress = function (event) {
 
   //Creating variables for use on keypress event
   var userGuess = event.key;
   var userGuessKey1 = event.keyCode;
   var userGuessKey2 = event.which;
+  instructionText.textContent = ""
   var test
 
   //Determine if user pressed lowercase letter
   if ((userGuessKey1 >= 97 || userGuessKey2 >= 97) && (userGuessKey1 <=122 || userGuessKey2 <= 122)){
-      if(guessLeft <= 8){
-        var randomLetter = ((stringGen((1))));
-        guessLeft = guessLeft++;
-        guessLeftText.textContent = "Guess left: " + guessLeft;
+    if(userGuess == randomLetter){
+      //Variables are updated and new character selected
+      wins++;
+      guessLeft = 9;
+      randomLetter = stringGen(1);
+      guessArray = [];
 
-        //Consule test logs
-        console.log(randomLetter);
-        console.log(userGuess);
-        console.log(Number(userGuess));
-        console.log(userGuessKey1);
-        console.log(userGuessKey2);
-        console.log(guessLeft);
-        console.log("---------------")
+      //Update Screen Text
+      instructionText.textContent = "Push any letter to start game."
+      winsText.textContent = "Wins: " + wins;
+      lossesText.textContent = "Losses: " + losses;    
+      guessLeftText.textContent = "Guesses left: " + guessLeft;
+      userGuessText.textContent = "Your guesses so far: ";
+
+    }else if (guessLeft <= 1){
+      //Variable are updated and new charater is selected 
+      losses++;
+      guessLeft = 9;
+      randomLetter = stringGen(1);
+      guessArray = [];
+      
+      //Update Screen Text
+      instructionText.textContent = "Push any letter to start game.";
+      lossesText.textContent = "Losses: " + losses;
+      winsText.textContent = "Wins: " + wins;
+      guessLeftText.textContent = "Guesses left: " + guessLeft;
+      userGuessText.textContent = "Your guesses so far: ";
+
     } else{
-      alert("you have used 9 guesses");
-    }
+      //Variables and array are updated
+      guessLeft--;
+      guessArray.push(userGuess);
+
+      //Update Screen Text
+      guessLeftText.textContent = "Guesses left: " + guessLeft;
+      userGuessText.textContent = "Your guesses so far : " + guessArray;
+    } 
   }
 }
